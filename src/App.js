0c45fb718e0f8591d17d25f528461239cc398ee8
Rob1 <robin.pautigny@hetic.net>
2022-01-25 00:07:06 +0100

    🦠

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import opensea from './img/opensea.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1 className='openingTitle'>Hello we are PandemicMasks 🦠</h1>
+        <a href='https://opensea.io/collection/pandemicmasks'>
+          <h2 className='linkOpenSea'>find us on opensea</h2> <img className='logoOpensea' src={opensea}/>
         </a>
+        
       </header>
     </div>
   );
